6e28ace86848b2aeab114e413f29d1efb2807e08
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-05-23 01:00:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '123565034455-vvrtn6n4m4odehrs118ei6rl776u5q90.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'LFdQaZNw5r0RXBSVYOA7NidM',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
